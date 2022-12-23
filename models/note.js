ada36aa28002e6950fd82e5c4933ae580cd0dc52
farrar <thaobaan@gmail.com>
2022-12-23 08:44:02 +0100

    refactored code and added user authentication and authorization to the application

--- models/note.js
+++ models/note.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5,
   },
-  date: {
-    type: Date,
-    required: true,
-  },
+  date: Date,
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
-
 noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
---
+++ tests/users_api.test.js
@@ -0,0 +1,48 @@
+const bcrypt = require("bcrypt");
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../app");
+const api = supertest(app);
+
+const helper = require("./test_helper");
+
+const User = require("../models/user");
+
+//...
+
+beforeEach(async () => {
+  await User.deleteMany({});
+
+  const passwordHash = await bcrypt.hash("sekret", 10);
+  const user = new User({ username: "root", passwordHash });
+
+  await user.save();
+});
+
+describe("when there is initially one user in db", () => {
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "mluukkai",
+      name: "Matti Luukkainen",
+      password: "salainen",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
